466fef91a8ed4582c966a128acabadc5ec5d68c6
Splleat <Splleat@gmail.com>
2026-04-09 13:24:36 +0900

    refactor: 사용하지 않는 인터페이스 삭제

--- src/main/java/com/nhnacademy/fbp/infrastructure/mqtt/MessageListener.java
+++
@@ -1,6 +0,0 @@
-package com.nhnacademy.fbp.infrastructure.mqtt;
-
-public interface MessageListener {
-    void onMessage(String topic, byte[] payload);
-    void onConnectionLost(Throwable cause);
-}
