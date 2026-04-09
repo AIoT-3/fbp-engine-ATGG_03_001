374c0c6fcded7e143a366f1b2261b2c5c1241937
Splleat <Splleat@gmail.com>
2026-04-09 13:19:51 +0900

    feat: MqttSubscriberNode에 자동 재연결 설정 및 잘못된 메시지 수신 처리 로직 추가

--- src/main/java/com/nhnacademy/fbp/node/mqtt/MqttSubscriberNode.java
+++ src/main/java/com/nhnacademy/fbp/node/mqtt/MqttSubscriberNode.java
@@ -2,62 +2,68 @@ package com.nhnacademy.fbp.node.mqtt;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.hivemq.client.mqtt.MqttClient;
 import com.hivemq.client.mqtt.MqttGlobalPublishFilter;
+import com.hivemq.client.mqtt.lifecycle.MqttDisconnectSource;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
 import com.hivemq.client.mqtt.mqtt5.message.publish.Mqtt5Publish;
-import com.nhnacademy.fbp.common.util.JsonParserUtils;
+import com.nhnacademy.fbp.common.util.JsonUtils;
 import com.nhnacademy.fbp.core.messsage.Message;
+import com.nhnacademy.fbp.core.node.ConnectionState;
 import com.nhnacademy.fbp.core.node.ProtocolNode;
 import lombok.extern.slf4j.Slf4j;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class MqttSubscriberNode extends ProtocolNode {
     private final Mqtt5BlockingClient client;
     private final String topic;
-    private final String brokerHost;
-    private final int brokerPort;
+    private final String serverHost;
+    private final int serverPort;
 
-    private MqttSubscriberNode(String id, String brokerHost, int brokerPort, String topic, long reconnectIntervalMs) {
-        super(id, reconnectIntervalMs);
+    private MqttSubscriberNode(String id, String serverHost, int serverPort, String topic) {
+        super(id);
         this.topic = topic;
-        this.brokerHost = brokerHost;
-        this.brokerPort = brokerPort;
+        this.serverHost = serverHost;
+        this.serverPort = serverPort;
 
         client = MqttClient.builder()
                 .useMqttVersion5()
                 .identifier(id)
-                .serverHost(brokerHost)
-                .serverPort(brokerPort)
+                .serverHost(serverHost)
+                .serverPort(serverPort)
+                .addConnectedListener(context -> setConnectionState(ConnectionState.CONNECTED))
+                .addDisconnectedListener(context -> {
+                    setConnectionState(ConnectionState.DISCONNECTED);
+
+                    context.getReconnector()
+                        .reconnect(context.getSource() != MqttDisconnectSource.USER)
+                        .delay(getReconnectIntervalMs(), TimeUnit.MILLISECONDS);
+                })
                 .buildBlocking();
 
         addOutputPort("out");
     }
 
     public static MqttSubscriberNode create(String id, String brokerHost, int brokerPort, String topic) {
-        return new MqttSubscriberNode(id, brokerHost, brokerPort, topic, 5000);
+        return new MqttSubscriberNode(id, brokerHost, brokerPort, topic);
     }
 
     @Override
-    public void connect() {
-        try {
-            client.connect();
-        } catch (Exception e) {
-            log.error("MQTT 연결 실패: {}", e.getMessage(), e);
-        }
+    protected void connect() throws Exception {
+        client.connect();
     }
 
     @Override
-    public void reconnect() {
-        log.info("MQTT 연결 재시도 중...");
+    protected void reconnect() {
+        // 자동 재연결 설정 사용
     }
 
     @Override
-    public void disconnect() {
+    protected void disconnect() {
         try {
             client.disconnect();
         } catch (Exception e) {
@@ -72,32 +78,34 @@ public class MqttSubscriberNode extends ProtocolNode {
 
     @Override
     public void run() {
+        String jsonRaw = null;
+
         try (Mqtt5BlockingClient.Mqtt5Publishes publishes = client.publishes(MqttGlobalPublishFilter.ALL)) {
             client.subscribeWith().topicFilter(topic).send();
 
-            log.info("MQTT 구독 시작 - 브로커: {}:{}, 토픽: {}", brokerHost, brokerPort, topic);
+            log.info("MQTT 구독 시작 - 브로커: {}:{}, 토픽: {}", serverHost, serverPort, topic);
 
             while (!Thread.currentThread().isInterrupted()) {
-                log.info("MQTT 클라이언트 - 연결 상태: {}", client.getState());
                 Mqtt5Publish publish = publishes.receive();
 
-                String payloadStr = new String(publish.getPayloadAsBytes(), StandardCharsets.UTF_8);
+                jsonRaw = new String(publish.getPayloadAsBytes(), StandardCharsets.UTF_8);
 
-                log.info("MQTT 메시지 수신 - 토픽: {}, 페이로드: {}", publish.getTopic(), payloadStr);
+                Map<String, Object> jsonMap = JsonUtils.get().readValue(jsonRaw, new TypeReference<>() {});
 
-                Map<String, Object> payload = JsonParserUtils.get().readValue(payloadStr, new TypeReference<>() {});
-
-                Message converted = Message.create(payload)
+                Message converted = Message.from(jsonMap)
                         .withEntry("topic", publish.getTopic().toString());
 
                 process(converted);
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-        } catch (JsonMappingException e) {
-            log.error("MQTT 메시지 JSON 매핑 오류: {}", e.getMessage(), e);
         } catch (JsonProcessingException e) {
             log.error("MQTT 메시지 JSON 처리 오류: {}", e.getMessage(), e);
+
+            Message raw = Message.create()
+                    .withEntry("rawPayload", jsonRaw);
+
+            process(raw);
         } catch (Throwable e) {
             log.error("알 수 없는 MQTT 오류: {}", e.getMessage(), e);
         }
