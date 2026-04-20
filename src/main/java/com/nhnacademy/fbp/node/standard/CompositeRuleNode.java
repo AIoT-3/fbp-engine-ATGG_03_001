09366d10936fdfc9f1bfb0e32c53a999bdea50d8
Splleat <Splleat@gmail.com>
2026-04-20 12:13:52 +0900

    feat: CompositeRuleNode 구현

---
+++ src/main/java/com/nhnacademy/fbp/node/standard/CompositeRuleNode.java
@@ -0,0 +1,45 @@
+package com.nhnacademy.fbp.node.standard;
+
+import com.nhnacademy.fbp.core.messsage.Message;
+import com.nhnacademy.fbp.core.node.AbstractNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class CompositeRuleNode extends AbstractNode {
+    private final Operator operator;
+    private final List<Predicate<Message>> conditions;
+
+    private CompositeRuleNode(String id, Operator operator) {
+        super(id);
+
+        this.operator = operator;
+        this.conditions = new ArrayList<>();
+
+        addInputPort("in");
+        addOutputPort("match");
+        addOutputPort("mismatch");
+    }
+
+    public static CompositeRuleNode create(String id, Operator operator) {
+        return new CompositeRuleNode(id, operator);
+    }
+
+    @Override
+    protected void onProcess(Message message) {
+        if (operator.evaluate(conditions, message)) {
+            send("match", message);
+        } else {
+            send("mismatch", message);
+        }
+    }
+
+    public void addCondition(Predicate<Message> condition) {
+        conditions.add(condition);
+    }
+
+    public void addCondition(String expression) {
+        conditions.add(RuleExpression.parse(expression));
+    }
+}
---
+++ src/main/java/com/nhnacademy/fbp/node/standard/Operator.java
@@ -0,0 +1,23 @@
+package com.nhnacademy.fbp.node.standard;
+
+import com.nhnacademy.fbp.core.messsage.Message;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@RequiredArgsConstructor
+public enum Operator {
+    AND((conditions, message) -> conditions.stream()
+                .allMatch(condition -> condition.test(message))),
+    OR(((conditions, message) -> conditions.stream()
+            .anyMatch(condition -> condition.test(message))));
+
+    private final LogicGate logicGate;
+
+    public boolean evaluate(List<Predicate<Message>> conditions, Message message) {
+        if (conditions == null || message == null) return false;
+
+        return logicGate.apply(conditions, message);
+    }
+}
